c26637ff123ec66a1d3833e2e58ffc37446b574a
talhaayub <ccntchstudio@gmail.com>
2026-05-07 23:07:01 +0500

    remove digits add meta tags

--- Header.astro_astro_type_script_index_1_lang.CuY9SSl3.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./utilities.DCWVKFoH.js";const l={id:"login-link",loggedInLabel:"My Digits"},g=l?.id,o=l?.loggedInLabel;i(g,o);
--- TOSLayout.astro_astro_type_script_index_0_lang.Cq0JRAYS.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./utilities.DCWVKFoH.js";e(document.querySelectorAll("h2, h3, h4, h5, h6"),"heading-link");
